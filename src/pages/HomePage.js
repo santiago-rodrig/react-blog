4b8454eb1a891a4767e46000ab47e5ecb581b823
santiago-rodrig <santo1996.29@gmail.com>
2021-02-25 16:54:57 -0400

    cleanup and initial content

---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <h1>Hello, welcome to my blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut
+        risus vitae arcu aliquam aliquam vitae eu massa. Aliquam viverra non
+        orci nec cursus. Fusce aliquam porttitor odio vel tempus. Fusce ligula
+        nunc, egestas vitae risus nec, vehicula rhoncus enim. Suspendisse
+        tincidunt imperdiet lacus at hendrerit. Duis euismod a libero nec
+        vestibulum. Duis hendrerit velit eget felis finibus commodo. Vestibulum
+        dictum tortor ut eros molestie, at maximus est interdum. In vehicula
+        vehicula finibus. Aenean dapibus laoreet arcu, at euismod purus faucibus
+        a. Quisque condimentum leo non tellus ullamcorper viverra. Ut quis metus
+        ornare, fringilla eros non, eleifend nunc.
+      </p>
+      <p>
+        Nam consequat ac magna sit amet euismod. Quisque hendrerit velit sapien,
+        et porta lacus viverra et. Maecenas ex magna, tincidunt quis magna ac,
+        gravida imperdiet justo. Etiam maximus bibendum ex eget porta. In hac
+        habitasse platea dictumst. Morbi egestas urna id libero malesuada
+        dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        egestas et diam id interdum. Morbi posuere dui eget urna aliquet
+        euismod. Nulla pulvinar est eu tellus dictum imperdiet. Curabitur
+        suscipit quis dui scelerisque bibendum. Nam pretium maximus diam. Cras
+        ut viverra velit, ut rhoncus eros. In condimentum sapien consectetur
+        eros venenatis convallis.
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
