7785e55ff1564d4ca320ecf240555e46bb17e575
santiago-rodrig <santo1996.29@gmail.com>
2021-02-25 20:40:28 -0400

    fetch article metadata in component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import axios from "axios";
 
 function ArticlePage({ match }) {
   const name = match.params.name;
-  const article = articleContent.find((article) => article.name === name);
+  const [loading, setLoading] = useState(true);
+  const article = articleContent.find((a) => a.name === name);
 
+  const [articleMetaData, setArticleMetaData] = useState({
+    upvotes: 0,
+    comments: [],
+    name: "",
+  });
+
+  useEffect(() => {
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+
+    async function getArticle() {
+      try {
+        const article = await axios.get(`/api/articles/${name}`, {
+          cancelToken: source.token,
+        });
+
+        setArticleMetaData(article);
+      } catch (error) {
+        console.error("Failed to fetch article", name);
+      }
+
+      setLoading(false);
+    }
+
+    getArticle();
+
+    return () => {
+      source.cancel();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) return <p>Loading article...</p>;
   if (!article) return <NotFoundPage />;
 
-  const otherArticles = articleContent.filter(
-    (article) => article.name !== name
-  );
+  const otherArticles = articleContent.filter((a) => a.name !== name);
 
   return (
     <>
-      <h1>{article.title}</h1>
+      <h1>{article.name}</h1>
+      <h3>{articleMetaData.upvotes} upvotes</h3>
       {article.content.map((paragraph, idx) => (
         <p key={idx}>{paragraph}</p>
       ))}
